f9d3d9c1e99ea9960242e8b481d2688b098b66ff
cipher-tech <nickchibuikem@gmail.com>
2022-03-23 12:26:36 +0100

    setup prettier

--- webpack.config.js
+++ webpack.config.js
@@ -5,25 +5,25 @@ const path = require('path');
 module.exports = {
     mode: 'development',
     devtool: 'inline-source-map',
-  entry: path.resolve(__dirname, './index.js'), // eslint-disable-line
-  module: {
-    rules: [
-      {
-        test: /\.(js)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader']
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['*', '.js']
-  },
-  output: {
-    filename: '[name].js',
-     path: path.resolve(__dirname, 'dist'), // eslint-disable-line
-     clean: true
-  },
-  devServer: {
-    static: path.resolve(__dirname, 'dist'), // eslint-disable-line
-  },
+    entry: path.resolve(__dirname, './index.js'), // eslint-disable-line
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader' ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js']
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'), // eslint-disable-line
+        clean: true
+    },
+    devServer: {
+        static: path.resolve(__dirname, 'dist') // eslint-disable-line
+    }
 };
