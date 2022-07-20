7713625748ec165fb7bbd98737712ab63a0ea6de
cipher-tech <nickchibuikem@gmail.com>
2022-07-20 14:48:20 +0100

    added authService test

---
+++ test/unit/services/authService.test.js
@@ -0,0 +1,9 @@
+import AuthService from "../../../app/services/authService";
+import "../../mock/mockUserModel";
+import "../../mock/mockLogger";
+
+describe("Test methods in AuthService", () => {
+    test("should register user :::registerService", async () => {
+
+    });
+});
