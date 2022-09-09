972716d8b02143305aea85461709d3226b4d3981
cipher-tech <nickchibuikem@gmail.com>
2022-09-09 21:53:41 +0100

    added more auth service tests

--- test/unit/services/authService.test.js
+++ test/unit/services/authService.test.js
@@ -2,29 +2,147 @@ import AuthService from "../../../app/services/authService/authService";
 import "../../../config/logger";
 import "../../mock/mockLogger";
 import "../../mock/mockUserModel";
+import UserService from "../../../app/services/userService/userService";
+import HashText from "../../../app/utils/hashText";
 
-describe.only("Test token service", () => {
-    jest.mock("../../../app/services/userService", () => jest.fn());
-
-    test("should generate token", async () => {
-        const user = {
-            email: "newuseruu@mail.com",
+describe("Test token service", () => {
+    let user;
+    beforeEach(() => {
+        jest.resetModules();
+        user = {
             password: "newpassword",
+            email: "newuseruu@mail.com",
             name: "cipher tech",
             phone_number: "06046363777",
             age: 28,
             date_of_birth: "1999-09-08"
         };
-        const authService = new AuthService();
-
-        const userData = await authService.registerService(user);
-        await expect(userData).not.toBeNull();
     });
-    // test("should fail to generate token(user object is null)", async () => {
-    //     const tokenService = new TokenService();
+    afterEach(() => {
+        jest.resetModules();
+    });
+    describe("Test register service", () => {
+        jest.mock("../../../app/services/userService", () => jest.fn());
+        test("should fail to generate token, invalid inputs: no password", async () => {
+            const newUser = {
+                ...user,
+                password: null
+            };
+            const authService = new AuthService();
 
-    //     const token = await tokenService.generateAuthToken;
-    //     await expect(token).rejects.toThrowError("We've encountered an issue. Please
-    // retry in a few minutes.");
-    // });
+            const userData = authService.registerService;
+
+            await expect(userData(newUser)).rejects.toThrow("An error occurred please try again or contact support.");
+        });
+        test("should create new user and generate token", async () => {
+            const userToCreate = {
+                id: 9,
+                name: "new cipher",
+                email: "newuser@mail.com",
+                phone_number: "06046363777",
+                age: 28,
+                date_of_birth: "1991-09-08",
+                created_at: "2022-07-09T16:55:11.936Z"
+            };
+            const authService = new AuthService();
+
+            const userData = await authService.registerService(user);
+            await expect(userData).not.toBeNull();
+            await expect(userData.userDetails).toEqual(userToCreate);
+            await expect(userData.token).toStrictEqual({
+                access: {
+                    token: expect.any(String),
+                    expires: expect.any(Date)
+                }
+            });
+        });
+
+        test("should fail to create new user(user already exists).", async () => {
+            const userToCreate = {
+                id: 9,
+                name: "new cipher",
+                password: "newPassword",
+                email: "newuseruu@mail.com",
+                phone_number: "06046363777",
+                age: 28,
+                date_of_birth: "1991-09-08",
+                created_at: "2022-07-09T16:55:11.936Z"
+            };
+
+            jest.spyOn(UserService.prototype, "getUserByEmail").mockImplementation(() => userToCreate);
+            const authService = new AuthService();
+
+            const userData = authService.registerService;
+
+            await expect(userData(userToCreate)).rejects.toThrow("User with email already exists");
+        });
+    });
+
+    describe("Test login service", () => {
+        jest.mock("../../../app/services/userService", () => jest.fn());
+        // jest.mock("../../../app/utils/hashText", () => jest.fn());
+        beforeEach(() => {
+            jest.resetModules();
+            user = {
+                password: "newpassword",
+                email: "newuseru@mail.com",
+                name: "cipher tech",
+                phone_number: "06046363777",
+                age: 28,
+                date_of_birth: "1999-09-08"
+            };
+            jest.spyOn(UserService.prototype, "getUserByEmail").mockImplementation(() => user);
+            jest.spyOn(HashText, "verifyHash").mockImplementation(() => null);
+        });
+        afterEach(() => {
+            jest.resetModules();
+        });
+        test("Should fail to login: no email or password", async () => {
+            const userToLoginNoEmail = {
+                password: "newPassword"
+            };
+            const authService = new AuthService();
+            const login = authService.loginService;
+
+            await expect(login(userToLoginNoEmail)).rejects.toThrow("Password and email are required");
+
+            const userToLoginNoPassword = {
+                email: "newuseruu@mail.com"
+            };
+
+            await expect(login(userToLoginNoPassword)).rejects.toThrow("Password and email are required");
+        });
+        test("Should fail to login: User doesn't exist", async () => {
+            const userToLogin = {
+                password: "newPassword",
+                email: "newuseruu@mail.com"
+            };
+            jest.spyOn(UserService.prototype, "getUserByEmail").mockImplementation(() => null);
+            const authService = new AuthService();
+            const login = authService.loginService;
+
+            await expect(login(userToLogin)).rejects.toThrow("Invalid email or password please try again");
+        });
+        test("Should fail to login: Passwords do not match", async () => {
+            const userToLogin = {
+                password: "newPass",
+                email: "newuser@mail.com"
+            };
+            jest.spyOn(HashText, "verifyHash").mockImplementation(() => null);
+            const authService = new AuthService();
+            const login = await authService.loginService;
+            await expect(login(userToLogin)).rejects.toThrow("Invalid email or password please try again");
+        });
+        test("Should login in use successfully", async () => {
+            const userToLogin = {
+                password: "newPass",
+                email: "newuser@mail.com"
+            };
+            const authService = new AuthService();
+            const login = await authService.loginService(userToLogin);
+
+            console.log(":::::::::::;(userToLogin) (userToLogin)", login);
+            await expect(login).rejects.toThrow("Invalid email or password please try again");
+        });
+    });
 });
