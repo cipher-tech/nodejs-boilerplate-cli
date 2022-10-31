e7b69f097ac2ef7974fe612f18df926decf883e4
cipher-tech <nickchibuikem@gmail.com>
2022-10-31 14:38:32 +0100

    refreshed dist folder

--- dist/lib/sequelize/javascript/express/app/services/tokenService/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const tokenService_1 = __importDefault(require("./tokenService"));
-exports.default = tokenService_1.default;
