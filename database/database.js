c14500fc601ff570dd84685ed98fd7d6441334c5
cipher-tech <nickchibuikem@gmail.com>
2022-12-10 21:59:08 +0100

    updated database config

--- database/database.js
+++ database/database.js
@@ -1,33 +1,34 @@
-// const process.env = require("../process.env");
+import config from "../config";
+
 // run source .env before running "npx sequelize-cli db:migrate" to set environment variables.
 
 module.exports = {
     development: {
-        username: process.env.USERNAME,
-        password: process.env.PASSWORD,
-        database: process.env.DATABASE,
-        host: process.env.HOST,
-        dialect: "postgres",
+        username: config.USERNAME,
+        password: config.PASSWORD,
+        database: config.DATABASE,
+        host: config.HOST,
+        dialect: config.DIALECT,
         dialectOptions: {
             bigNumberStrings: true
         }
     },
     test: {
-        username: process.env.USERNAME,
-        password: process.env.PASSWORD,
-        database: process.env.DATABASE,
-        host: process.env.HOST,
-        dialect: "postgres",
+        username: config.USERNAME,
+        password: config.PASSWORD,
+        database: config.DATABASE,
+        host: config.HOST,
+        dialect: config.DIALECT,
         dialectOptions: {
             bigNumberStrings: true
         }
     },
     production: {
-        username: process.env.USERNAME,
-        password: process.env.PASSWORD,
-        database: process.env.DATABASE,
-        host: process.env.HOST,
-        dialect: "postgres",
+        username: config.USERNAME,
+        password: config.PASSWORD,
+        database: config.DATABASE,
+        host: config.HOST,
+        dialect: config.DIALECT,
         dialectOptions: {
             bigNumberStrings: true
         }
