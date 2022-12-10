5eb94f4346826ed8b8377e467a21ea54bf5fc94c
cipher-tech <nickchibuikem@gmail.com>
2022-12-10 19:01:24 +0100

    removed test file

--- prohect/app/services/authService/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-/* eslint-disable import/prefer-default-export */
-const authService_1 = __importDefault(require("./authService"));
-exports.default = authService_1.default;
