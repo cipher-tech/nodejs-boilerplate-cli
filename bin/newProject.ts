bcf0f2be67522203b2f6f2a0838b9349610ea4ed
cipher-tech <nickchibuikem@gmail.com>
2022-12-24 00:00:13 +0100

    added boilerplate config settings

--- bin/newProject.ts
+++ bin/newProject.ts
@@ -1,7 +1,7 @@
 import inquirer from "inquirer";
 import fsExtra from "fs-extra";
 import fs from "fs";
-import {promisify} from 'util'
+import { promisify } from 'util'
 import ora from "ora";
 import child_process from "child_process";
 import chalk from "chalk";
@@ -79,26 +79,26 @@ class NewProject {
         return framework[ option ]
     }
     async generateBoilerplate(options: IGenerateBoilerplateOptions) {
-       const fsAccess =  promisify(fs.access)
-       const fsRename =  promisify(fs.rename)
+        const fsAccess = promisify(fs.access)
+        const fsRename = promisify(fs.rename)
         const { name } = options
         try {
             console.log(":::::::;options options", options);
             const driver = this.verifyDrivers(options.driver)
             const language = this.verifyLanguage(options.language)
             const framework = this.verifyFramework(options.framework)
-            
-            const repoToClone = `${driver}/${language}/${framework}`
+
+            const repoToClone = `${ driver }/${ language }/${ framework }`
             console.log("::::::::::: verifyLanguage", {
                 driver,
                 language,
                 framework
             });
-            const cloneRepo = child_process.spawn(`git clone ${ repos[repoToClone] }`, {
+            const cloneRepo = child_process.spawn(`git clone ${ repos[ repoToClone ] }`, {
                 shell: true,
             })
 
-            cloneRepo.on('error', () => {
+            await cloneRepo.on('error', () => {
                 npmInstallSpinner.fail(
                     chalk.red(
                         `
@@ -108,32 +108,56 @@ class NewProject {
                 );
             });
 
-            cloneRepo.on('close', async () => {
-                npmInstallSpinner.succeed(chalk.green(`Packages installed successfully`));
+            return await new Promise((resolve, reject) => {
+                cloneRepo.on('close', async () => {
+                    npmInstallSpinner.succeed(chalk.green(`Packages installed successfully`));
 
-                const hasAccess: any = await fsAccess("enyata-node-base", fs.constants.F_OK)
-                console.log(":::::::: hasAccess hasAccess", hasAccess);
-                
+                    const hasAccess: any = await fsAccess("enyata-node-base", fs.constants.F_OK)
+                    console.log(":::::::: hasAccess hasAccess", hasAccess);
+
                     if (!hasAccess) {
                         await fsRename('enyata-node-base', name)
                     } else {
                         console.log('The file already exists');
                     }
-                console.log(
-                    chalk.green(` Repo cloned successfully!!!... `)
-                );
-            });
+                    console.log(
+                        chalk.green(` Repo cloned successfully!!!... `)
+                    );
 
-            return true
+                    resolve(true)
+                });
+
+            })
 
         } catch (error) {
             console.log("There was an error while generating boilerplate");
             throw error
         }
     }
+    async addConfigFile(options: IGenerateBoilerplateOptions) {
+        const { name } = options
+        const fsAccess = promisify(fs.access)
 
+        try {
+            console.log(chalk.cyan("Generating CLI config file ..."));
+
+            console.log("::::::::::;; configToString", options);
+            const hasAccess: any = await fsAccess(name, fs.constants.F_OK)
+            if (!hasAccess) {
+                await fsExtra.writeJSON(`./${ name }/.clirc.json`, options)
+            } else {
+                console.log('The folder doesn\'t exists');
+            }
+
+            console.log(chalk.cyan("Finished generating CLI config file."));
+        } catch (error) {
+            console.log(chalk.cyan("An error while generating CLI config file."));
+            console.log(error);
+        }
+
+    }
     async installDependencies(name: string) {
-        npmInstallSpinner.start(chalk.cyan(`Installing dependencies packages for ${ name }`));
+        npmInstallSpinner.start(chalk.cyan(`Installing dependencies for ${ name }`));
         const { default: child_process } = await import("child_process");
         const childProcess = child_process.spawn(`cd ${ name } && npm install`, {
             shell: true,
@@ -143,8 +167,9 @@ class NewProject {
             npmInstallSpinner.fail(
                 chalk.red(
                     `
-                An error occurred, please try again. 
-                If problem persist please raise an issue on Github`
+                    An error occurred, please try again. 
+                    If problem persist please raise an issue on Github
+                `
                 )
             );
         });
@@ -161,7 +186,7 @@ class NewProject {
                🚀🚀🚀🚀🚀
         
             For How to use and more info on cli
-            Visit  ...userOptions
+            Visit  ..
             Cheers!!!
         
             `)
@@ -175,11 +200,12 @@ class NewProject {
                 return new Error("Error while resolving driver or language")
             }
             creatingProjectSpinner.start(chalk.cyan(`Creating ${ name }`));
-
             console.log(chalk.cyan("Generating Boilerplate ..."));
             const generatedProject = await this.generateBoilerplate({ name, ...userOptions })
-            console.log("Boilerplate generated successfully  ...");
+            console.log("Boilerplate generated successfully.");
             creatingProjectSpinner.succeed(chalk.green(`${ name } created successfully`));
+
+            await this.addConfigFile({ name, ...userOptions })
 
             await this.installDependencies(name)
             return userOptions;
