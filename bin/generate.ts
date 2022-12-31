0f25223ccb0a5a922c872ec4ea19b28729651060
cipher-tech <nickchibuikem@gmail.com>
2022-12-31 11:59:29 +0100

    added make controller function and refactored code

--- bin/generate.ts
+++ bin/generate.ts
@@ -3,22 +3,30 @@ import fs from "fs";
 import path from "path";
 
 import { IGenerateCliOptions } from ".";
+import { IConfigOptions } from "./constants";
 import { getCliConfig, fileExists as fileAccess, createFile } from "./util/helper";
 
 export class Generate {
+    formatConfigOptions(config: IConfigOptions) {
+        let { language: lang, driver: dbDriver, framework: projectFramework } = config
 
-    async model(options: IGenerateCliOptions, config: any) {
+        return {
+            language: lang.toLowerCase(),
+            driver: dbDriver.toLowerCase(),
+            framework: projectFramework.toLowerCase(),
+            extension: lang.toLowerCase() === "javascript" ? "js" : "ts",
+        }
+    }
+    async makeModel(options: IGenerateCliOptions, config: IConfigOptions) {
+
         try {
             const { model = null } = options
-            let { language: lang, driver: dbDriver, framework: projectFramework } = config
-
-            const language = lang.toLowerCase();
-            const driver = dbDriver.toLowerCase();
-            const framework = projectFramework.toLowerCase();
-            const extension = language === "javascript" ? "js" : "ts";
             if (!model) {
                 return;
             }
+            console.log(chalk.green(`Creating model ${ model }`));
+
+            let { language, driver, framework, extension } = this.formatConfigOptions(config);
 
             const source = path.resolve(__dirname, `./../../lib/${ driver }/${ language }/${ framework }/model/template.js`);
             const destination = `./database/models/${ model }.${ extension }`;
@@ -26,11 +34,12 @@ export class Generate {
             await createFile(model, source, destination)
 
             const generatedModelTemplate = fs.readFileSync(destination).toString();
-            
+
             let updatedModelTemplate = generatedModelTemplate.replace(/user/g, model);
             updatedModelTemplate = updatedModelTemplate.replace(/User/g, (model || "").charAt(0).toLocaleUpperCase() + (model || "").slice(1))
 
             fs.writeFileSync(destination, updatedModelTemplate);
+            console.log(chalk.green(`Finished creating model ${ model }`));
             return true
         } catch (error) {
             console.log(chalk.red(`An error occurred while generating model file.`));
@@ -38,10 +47,21 @@ export class Generate {
         }
     }
 
+    async makeController(options: IGenerateCliOptions, config: IConfigOptions) {
+        const { controller = null } = options
+        if (!controller) {
+            return;
+        }
+        console.log(chalk.green(`Creating controller ${ controller }`));
+        let { language, driver, framework, extension } = this.formatConfigOptions(config);
+        console.log(":::::::; in makeController", { language, driver, framework, controller, extension });
+
+    }
     async run(options: any) {
         try {
             const config = await getCliConfig();
-            await this.model(options, config);
+            await this.makeModel(options, config);
+            await this.makeController(options, config);
 
             return;
         } catch (error: any) {
