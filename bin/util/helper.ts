7c900b124a63f81bf23b61d6832292c7e9a7dcce
cipher-tech <nickchibuikem@gmail.com>
2023-01-08 01:08:54 +0100

    added function to add import to index file

--- bin/util/helper.ts
+++ bin/util/helper.ts
@@ -37,11 +37,51 @@ export const fileExists = async (path: string) => {
 }
 
 export const createFile = async (fileName: string, source: string, destination: string) => {
+    if (!fileName || !source || !destination) {
+        console.log(chalk.cyan(`An error occurred while creating file`));
+        throw new Error(`An error occurred while creating file`);
+    }
+
     const fileAccess = await fileExists(destination)
+
     if (fileAccess) {
         console.log(chalk.cyan(`File with name: '${ fileName }' already exists`));
         throw new Error(`File with name: '${ fileName }' already exists`);
     }
     await fsExtra.copy(source, destination);
+    return true
+}
+
+export const addImportToIndexFile = (destination: string, extension: string, fileName: string) => {
+    const data = fs.readFileSync(destination).toString().split('\n');
+    let done = false;
+    let addedImport = false
+    let addedExport = false
+    let importName = fileName.split('/').slice(-1)
+    data.forEach((item, index) => {
+        if (item.includes('import') &&
+            addedImport === false &&
+            index !== 0
+        ) {
+            const newImport = `import ${ importName }Class from './${ fileName }';`;
+
+            data.splice(index, 0, newImport);
+            addedImport = true;
+        }
+        if (item.includes('export') &&
+            done === false &&
+            addedExport === false &&
+            index !== 0
+        ) {
+            const newExport = `export const ${ importName } = ${ importName }Class;`;
+            data.splice(index, 0, newExport);
+
+            addedExport = true;
+            done = true;
+        }
+    });
+    const addNewImport = data.join('\n');
+    fs.writeFileSync(`./app/http/controllers/index.${ extension }`, addNewImport);
+    return true
 }
 
