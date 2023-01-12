1755cfa3e65432ba4f3a855876b23bd9ef7dae16
cipher-tech <nickchibuikem@gmail.com>
2023-01-12 13:21:15 +0100

    added generate route option

--- dist/bin/index.js
+++ dist/bin/index.js
@@ -45,6 +45,7 @@ program.command('make')
     .option('-m, --model <string>', 'Generate a model file')
     .option('-c, --controller <string>', 'Generate a controller file')
     .option('-s, --service <string>', 'Generate a service file')
+    .option('-r, --route <string>', 'Generate a route file')
     .action((options) => __awaiter(void 0, void 0, void 0, function* () {
     const generate = new generate_1.Generate();
     console.log("::::::: make", { options });
