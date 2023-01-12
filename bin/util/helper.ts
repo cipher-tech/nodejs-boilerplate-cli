f6ce6b1de438fa7cab05ec90d1bd88fb750ab698
cipher-tech <nickchibuikem@gmail.com>
2023-01-12 14:58:50 +0100

    refactored code

--- bin/util/helper.ts
+++ bin/util/helper.ts
@@ -39,6 +39,18 @@ export const fileExists = async (path: string) => {
     }
 }
 
+export const createIndexFileInFolder = async (directory: string) => {
+    const writeContent = util.promisify(fs.writeFile)
+
+    const indexFileExist = await fileExists(directory)
+    if (!indexFileExist) {
+        await new Promise((resolve, reject) => {
+            resolve(writeContent(directory, '// index file'))
+        })
+    }
+    return true;
+}
+
 export const createFile = async (fileName: string, source: string, destination: string) => {
     if (!fileName || !source || !destination) {
         console.log(chalk.cyan(`An error occurred while creating file`));
@@ -69,7 +81,7 @@ export const addImportToIndexFile = (destination: string, extension: string, fil
         data.push(newExport);
     } else {
         for (let i = 0; i < data.length; i++) {
-            let item = data[i];
+            let item = data[ i ];
             if (item.includes('import') &&
                 addedImport === false &&
                 i !== 0
@@ -94,6 +106,45 @@ export const addImportToIndexFile = (destination: string, extension: string, fil
     }
 
     fs.writeFileSync(destination, data.join('\n'));
+    return true
+}
+
+type IGenerateFile = {
+    destination: string,
+    source: string,
+    filename: string,
+    extension: string,
+    placeholder: string[],
+    addIndex: boolean
+}
+export const generateFile = async (options: IGenerateFile) => {
+    const { destination, source, filename, placeholder, addIndex = false, extension } = options;
+    // get the destination folder ./test/test.js === ./test/
+    let destinationFolder: string | string[] = destination.split("/");
+    destinationFolder.pop();
+
+    destinationFolder = destinationFolder.join('/');
+
+    // copy file from source to destination
+    await createFile(filename, source, destination)
+
+    let generatedModelTemplate = fs.readFileSync(destination).toString();
+
+    // replace placeholders in generated file
+    for(let value of placeholder){
+        generatedModelTemplate = generatedModelTemplate.replace(new RegExp(value, 'g'), capitalize(filename.split('/').slice(-1).join()))
+    }
+
+    fs.writeFileSync(destination, generatedModelTemplate);
+
+    if(addIndex){
+        const indexFileLocation = `${ destinationFolder }/index.${ extension }`;
+        // check if index file exists in folder
+        await createIndexFileInFolder(indexFileLocation);
+        // import file in index folder
+        addImportToIndexFile(indexFileLocation, extension, filename)
+
+    }
     return true
 }
 
